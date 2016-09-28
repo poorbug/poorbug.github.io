e93afbed5aa79cacc3c9c9432aeebec15613e083
baixiang.li <baixiang.li@speedx.com>
2016-09-29 01:23:32 +0800

    index

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,23 +14,4 @@ export const createRoutes = (store) => ({
     CounterRoute(store)
   ]
 })
-
-/*  Note: childRoutes can be chunked or otherwise loaded programmatically
-    using getChildRoutes with the following signature:
-
-    getChildRoutes (location, cb) {
-      require.ensure([], (require) => {
-        cb(null, [
-          // Remove imports!
-          require('./Counter').default(store)
-        ])
-      })
-    }
-
-    However, this is not necessary for code-splitting! It simply provides
-    an API for async route definitions. Your code splitting should occur
-    inside the route `getComponent` function, since it is only invoked
-    when the route exists and matches.
-*/
-
 export default createRoutes
