bffcb8f92cefe1317fe79f6f56b213723afcac44
baixiang.li@speedx.com <baixiang.li@speedx.com>
2016-11-20 19:03:43 +0800

    build

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,15 +9,7 @@ export const createRoutes = (store) => ({
   	{
   		path: '/',
   		component: Home
-  	},
-  	{
-  		path: 'me',
-  		component: Me
-  	},
-    {
-      path: 'app',
-      component : App
-    }
+  	}
   ]
 })
 export default createRoutes
