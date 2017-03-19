1e3ac25845325683b626ca5a28fad05ee4b8e25a
baixiang.li@speedx.com <baixiang.li@speedx.com>
2017-03-19 16:08:33 +0800

    non-header in home page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,7 @@ import s from './style'
 
 export default class Header extends React.Component {
   render() {
+    if (window.location.pathname === '/') return null
     return (
       <header className={s.header}>
         <Link to='/' >🚲 Poorbug</Link>
