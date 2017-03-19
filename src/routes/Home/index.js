ed0af51bc095d758f31f471ac1f5f93b85cc0a41
baixiang.li@speedx.com <baixiang.li@speedx.com>
2017-03-19 16:30:16 +0800

    home page -> mobile

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
 import { Link } from 'react-router'
+import React from 'react'
 import s from './style'
 
 export default class Home extends React.Component {
@@ -7,7 +7,7 @@ export default class Home extends React.Component {
     return (
       <div className={s.box}>
       	<h1>Coding Muscle</h1>
-      	<p><Link to='me' >Poorbug</Link></p>
+      	<p><Link to='me' >🚲 Poorbug</Link></p>
       	<ul>
           <li><Link to='app' >新 SpeedX App 页面开发总结</Link></li>
           <li><Link to='getup' >React 博客工程 上线</Link></li>
