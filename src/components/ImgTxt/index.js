23b3ff4e1a6af9ea6de92a50dece62d055024007
baixiang.li@speedx.com <baixiang.li@speedx.com>
2017-03-20 17:39:05 +0800

    图片点击放大

--- src/components/ImgTxt/index.js
+++ src/components/ImgTxt/index.js
@@ -1,12 +1,32 @@
+import Dialog from 'react-toolbox/lib/dialog'
 import React from 'react'
 import s from './style'
 
 export default class ImgTxt extends React.Component {
+  state = {
+    active: false
+  }
+
+  handleToggle = () => {
+    this.setState({ active: !this.state.active })
+  }
+
   render() {
     const { img, txt } = this.props
     return (
       <div className={s.imgtxt}>
-        <img src={img} />
+        <Dialog
+          actions={this.actions}
+          active={this.state.active}
+          onEscKeyDown={this.handleToggle}
+          onOverlayClick={this.handleToggle}
+          title={txt}
+          type='normal'
+          className={s.dialog}
+        >
+          <img src={img} />
+        </Dialog>
+        <img src={img} onClick={this.handleToggle}/>
         <br />
         <p>{txt}</p>
       </div>
